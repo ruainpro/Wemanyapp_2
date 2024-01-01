b2efd0d5dca62ad9eda1b4df0618b09067a2c03e
ruainpro <ruainpro@gmail.com>
2024-01-01 00:04:35 -0500

    Basic Project Setup

---
+++ src/main/java/com/dao/wethemany/WeTheManApp2Application.java
@@ -0,0 +1,13 @@
+package com.dao.wethemany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeTheManApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeTheManApp2Application.class, args);
+	}
+
+}
