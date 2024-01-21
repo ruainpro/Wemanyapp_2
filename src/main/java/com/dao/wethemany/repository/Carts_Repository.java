b72292eb2ef91b9a1355a2407f1a087a63a35dd9
ruainpro <ruainpro@gmail.com>
2024-01-21 00:24:42 -0500

    Carts and Product Repository

---
+++ src/main/java/com/dao/wethemany/repository/Carts_Repository.java
@@ -0,0 +1,23 @@
+package com.dao.wethemany.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dao.wethemany.models.Carts;
+
+
+@Repository
+public interface Carts_Repository extends MongoRepository<Carts, String>{
+
+	List<Carts> findByCartedBy(String email);
+
+	Carts findByIdAndCartedBy(String id, String email);
+
+	void deleteByIdAndCartedBy(String id, String email);
+
+	List<Carts> findByCartedByAndCartsStatus(String email, String string);	
+	
+
+}
